7ee659caa2f7ba7bd373eb8fa4b0af46363b1dc3
Fabricio Luiz <127236426+Fabricio-luiz@users.noreply.github.com>
2023-07-10 14:16:37 -0300

    Update main.js
    inclusão de variavel no escopo da função sbmit

--- exercicio_html_js/main.js
+++ exercicio_html_js/main.js
@@ -1,16 +1,18 @@
 const form = document.getElementById('form-valid');
 
-function validarFormulario(campoA, campoB) {
-    return campoB > campoA
+function validarFormulario(valorA, valorB) {
+    return valorB > valorA
 }
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function(e) {
     e.preventDefault();
-    
 
-    if (campoB == campoA) {
+let valorA = parseFloat (document.getElementById('campoA').value);
+let valorB = parseFloat (document.getElementById('campoB').value);
+
+    if (valorB == valorA) {
         alert("Não é válido pois B é igual a A!");
-    } else if (campoB > campoA) {
+    } else if (valorB > valorA) {
         alert("É valido pois B é maior que A!");
     } else {
         alert("Não é válido pois A é maior que B!");
