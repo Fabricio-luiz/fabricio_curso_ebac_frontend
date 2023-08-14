731496991ed29723ee24dc7bfb8ca0ff5468876b
Fabricio <fabricioflr@gmail.com>
2023-08-14 12:32:41 -0300

    inittial commit

--- exercicio_html_js/main.js
+++ exercicio_html_js/main.js
@@ -1,20 +1,29 @@
 const form = document.getElementById('form-valid');
+const campoA = document.getElementById('campoA')
+const campoB = document.getElementById('campoB')
 
-function validarFormulario(campoA, campoB) {
-    return campoB > campoA
+function validarFormulario(valorA, valorB) {
+    return valorB > valorA
 }
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function(e) {
+    let formValid
     e.preventDefault();
-    
 
-    if (campoB == campoA) {
-        alert("Não é válido pois B é igual a A!");
-    } else if (campoB > campoA) {
-        alert("É valido pois B é maior que A!");
+const valorA = document.getElementById('campoA').value;
+const valorB = document.getElementById('campoB').value;
+const menssagemSucesso = `É válido, o valor B: ${valorB} é maior que o valor A: ${valorA}`;
+
+    formValid = validarFormulario(valorA, valorB)
+    if (formValid) {
+        alert(menssagemSucesso);
+    } 
+    else if (valorB == valorA) {
+        alert("Não é valido pois B é igual A!");
     } else {
         alert("Não é válido pois A é maior que B!");
     }
-})
 
-console.log(form);  
+    campoA.value = '';
+    campoB.value = '';
+})
---
+++ exercicio_plugins_jquery/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: 'xxx.xxx.xxx-xx'
+    })
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(xx) xxxxx-xxxx'
+    })
+    $('#cep').mask('00000-000', {
+        placeholder: 'xxxxx-xxx'
+    })
+
+}
