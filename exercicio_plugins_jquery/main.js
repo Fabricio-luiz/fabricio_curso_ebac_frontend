4488596fcf98208eee93d0056c8372701e12a957
Fabricio <fabricioflr@gmail.com>
2023-08-14 15:12:35 -0300

    atualization

--- exercicio_plugins_jquery/main.js
+++ exercicio_plugins_jquery/main.js
@@ -9,4 +9,45 @@ $(document).ready(function(){
         placeholder: 'xxxxx-xxx'
     })
 
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            cpf: {
+                required: true  
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            enderecoCompleto: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+        },
+
+        messages: {
+            nome:'<span class="message-error">Por favor, insira seu nome</span>',
+            cpf:'<span class="message-error">Este campo é obrigatório</span>',
+            email:'<span class="message-error">Este campo é obrigatório</span>',
+            telefone:'<span class="message-error">Este campo é obrigatório</span>',
+            enderecoCompleto: '<span class="message-error">Este campo é obrigatório</span>',
+            cep: '<span class="message-error">Este campo é obrigatório</span>',
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existe ${camposIncorretos} campo(s) incorreto(s)`)
+            }
+        }
+    })
 })
