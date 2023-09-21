64aa9b1bf823d7e5787125a3dd18d4d1f9d0d978
Fabricio <fabricioflr@gmail.com>
2023-09-21 15:27:33 -0300

    initial commit

--- exercicio_bootstrap/main.js
+++ exercicio_bootstrap/main.js
@@ -1,21 +1,14 @@
-$(document).ready(function(){
-    $('#cpf').mask('000.000.000-00', {
-        placeholder: 'xxx.xxx.xxx-xx'
-    })
+$(document).ready(function() {
     $('#telefone').mask('(00) 00000-0000', {
         placeholder: '(xx) xxxxx-xxxx'
     })
-    $('#cep').mask('00000-000', {
-        placeholder: 'xxxxx-xxx'
+
     })
 
     $('form').validate({
         rules: {
             nome: {
                 required: true
-            },
-            cpf: {
-                required: true  
             },
             email: {
                 required: true,
@@ -24,21 +17,16 @@ $(document).ready(function(){
             telefone: {
                 required: true
             },
-            enderecoCompleto: {
-                required: true
-            },
-            cep: {
-                required: true
+            mensagem: {
+                required: true 
             },
         },
 
         messages: {
             nome:'<span class="message-error">Por favor, insira seu nome</span>',
-            cpf:'<span class="message-error">Este campo é obrigatório</span>',
             email:'<span class="message-error">Este campo é obrigatório</span>',
             telefone:'<span class="message-error">Este campo é obrigatório</span>',
-            enderecoCompleto: '<span class="message-error">Este campo é obrigatório</span>',
-            cep: '<span class="message-error">Este campo é obrigatório</span>',
+            mensagem: '<span class="message-error">Este campo é obrigatório</span>',
         },
         submitHandler: function(form) {
             alert('Formulário cadastrado')
@@ -51,4 +39,3 @@ $(document).ready(function(){
             }
         }
     })
-})
