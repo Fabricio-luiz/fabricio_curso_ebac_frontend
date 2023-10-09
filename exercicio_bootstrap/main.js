b56b2cf2346f2e36fc87328c5c777d4a65cc7e7e
Fabricio <fabricioflr@gmail.com>
2023-10-09 11:24:23 -0300

    initial commit

--- exercicio_bootstrap/main.js
+++ exercicio_bootstrap/main.js
@@ -33,13 +33,25 @@ $(document).ready(function() {
             aceitoTermos:'<span class="message-error">Este campo é obrigatório</span>',
         },
         submitHandler: function(form) {
-            alert('Formulário cadastrado')
+            swal({
+                title:  "Obrigado!",
+                text:   "Formulário cadastrado",
+                icon:   "success",
+            });
+            /*alert('Formulário cadastrado')*/
             form.reset();
         },
         invalidHandler: function(evento, validador) {
             let camposIncorretos = validador.numberOfInvalids();
             if (camposIncorretos) {
-                alert(`Existe ${camposIncorretos} campo(s) incorreto(s)`)
+                /*swal({
+                    title:  "Atenção!",
+                    text:   "campo(s) incorreto(s)",
+                    icon:   "error",
+                });*/
+                /*swal("Good job!", "You clicked the button!", "success");*/
+                swal("Atenção", `${camposIncorretos} campo(s) incorreto(s)`, "error");
+            /* alert(`Existe ${camposIncorretos} campo(s) incorreto(s)`)*/
             }
         }
     })
